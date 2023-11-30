9c6ab1855043a9f1abecff5568b97092f390b82f
shafi <shafi.bahrami.2015@gmail.com>
2023-11-30 18:19:22 +0100

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Navbar from './components/Nav';
+
+const router = createBrowserRouter([
+  {
+    path: "/houses",
+    element: <div>Houses</div>,
+  },
+  {
+    path: "/persons",
+    element: <div>Persons</div>,
+  },
+  {
+    path: "/quotes",
+    element: <div>Quotes</div>,
+  },
+  {
+    path:'*',
+    element:<div>Houses</div>
+  }
+]);
 
 function App() {
   return (
     <div>
-      App
+      <Navbar/>
+      <RouterProvider router={router}/>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import Footer from './components/Footer.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+       <App/>
+       <Footer/>
+    </ChakraProvider>
   </React.StrictMode>,
 )
